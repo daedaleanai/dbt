a89e52eaf6a90151d34ef0a49efded0498e7ca89
Johannes Frohnhofen <jf@daedalean.ai>
2021-02-10 16:23:03 +0100

    Changed version to v0.1.4

--- cmd/build.go
+++ cmd/build.go
@@ -24,7 +24,7 @@ import (
 const buildDirName = "BUILD"
 const buildFileName = "BUILD.go"
 const buildFilesDirName = "buildfiles"
-const dbtModulePath = "github.com/daedaleanai/dbt v0.1.3"
+const dbtModulePath = "github.com/daedaleanai/dbt v0.1.4"
 const initFileName = "init.go"
 const mainFileName = "main.go"
 const modFileName = "go.mod"
