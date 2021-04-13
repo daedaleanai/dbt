7fb51d9b4dfa2b00c59fae6ee441107be90f640b
Johannes Frohnhofen <jf@daedalean.ai>
2021-04-13 11:50:52 +0200

    Version v0.4 (#50)
    * move all rules to dbt-rules
    
    * move all rules to dbt-rules

--- cmd/build.go
+++ cmd/build.go
@@ -25,7 +25,6 @@ import (
 
 const buildDirName = "BUILD"
 const buildFileName = "BUILD.go"
-const dbtModulePath = "github.com/daedaleanai/dbt"
 const generatorDirName = "GENERATOR"
 const generatorOutputFileName = "output.json"
 const initFileName = "init.go"
@@ -42,12 +41,7 @@ const initFileTemplate = `
 
 package %s
 
-import (
-	"path"
-	"reflect"
-
-	"dbt/RULES/core"
-)
+import "dbt-rules/RULES/core"
 
 type __internal_pkg struct{}
 
@@ -56,7 +50,7 @@ func DbtMain(registerTargetFn func(core.OutPath, string, interface{})) {
 }
 
 func in(name string) core.Path {
-	return core.NewInPath(path.Join(reflect.TypeOf(__internal_pkg{}).PkgPath(), name))
+	return core.NewInPath(__internal_pkg{}, name)
 }
 
 func ins(names ...string) []core.Path {
@@ -68,7 +62,7 @@ func ins(names ...string) []core.Path {
 }
 
 func out(name string) core.OutPath {
-	return core.NewOutPath(path.Join(reflect.TypeOf(__internal_pkg{}).PkgPath(), name))
+	return core.NewOutPath(__internal_pkg{}, name)
 }
 `
 
@@ -77,15 +71,9 @@ const mainFileTemplate = `
 
 package main
 
-import "dbt/RULES/core"
+import "dbt-rules/RULES/core"
 
 %s
-
-type context interface {
-	core.Context
-	Initialize()
-	AddTarget(name string, target interface{}, cwd core.OutPath)
-}
 
 func main() {
 	core.GeneratorMain([]func(func(core.OutPath, string, interface{})){
@@ -366,9 +354,6 @@ func createModFileContent(moduleName string, modules map[string]string, pathPref
 		fmt.Fprintf(&mod, "require %s v0.0.0\n", modName)
 		fmt.Fprintf(&mod, "replace %s => %s/%s\n\n", modName, pathPrefix, modName)
 	}
-
-	fmt.Fprintf(&mod, "require dbt v0.0.0\n")
-	fmt.Fprintf(&mod, "replace dbt => %s %s\n\n", dbtModulePath, dbtVersion)
 
 	return []byte(mod.String())
 }
