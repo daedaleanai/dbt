0f2d4f479c68ce1ea1f97baff7d5342ee410f51f
Johannes Frohnhofen <jf@daedalean.ai>
2021-09-20 18:35:46 +0200

    dbt run, fixes #18

---
+++ cmd/run.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/daedaleanai/cobra"
+)
+
+var runCmd = &cobra.Command{
+	Use:   "run [patterns] [build flags] [: run args]",
+	Short: "Builds and runs the targets",
+	Long:  `Builds and runs the targets.`,
+	Run:   runRun,
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return completeBuildArgs(toComplete, true), cobra.ShellCompDirectiveNoFileComp
+	},
+	DisableFlagsInUseLine: true,
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+}
+
+func runRun(cmd *cobra.Command, args []string) {
+	runArgs := []string{}
+	buildArgs := args
+	for idx, arg := range args {
+		if arg == ":" {
+			runArgs = args[idx+1:]
+			buildArgs = args[:idx]
+			break
+		}
+	}
+	runBuild(buildArgs, true, runArgs)
+}
