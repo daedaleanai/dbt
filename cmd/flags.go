a865c064e091a1d69ddcdc8d91d297c5a28fde9e
Niels Haandbaek <nh@daedalean.ai>
2023-07-17 11:45:13 +0200

    Updated command description based on feedback.

--- cmd/flags.go
+++ cmd/flags.go
@@ -6,8 +6,8 @@ import (
 
 var flagsCmd = &cobra.Command{
 	Use:   "flags [build flags]",
-	Short: "Lists all flags.",
-	Long:  `Lists all flags.`,
+	Short: "Lists all flags",
+	Long:  `Lists all flags and optionally updates any/all of them.`,
 	Run:   runFlags,
 	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		return completeBuildArgs(toComplete, modeList), cobra.ShellCompDirectiveNoFileComp
