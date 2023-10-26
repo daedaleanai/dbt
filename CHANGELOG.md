89ffbedba6add83d0abfea9e35389981f7216e9f
Ivan Egorov <ie@daedalean.ai>
2023-10-26 13:49:27 +0200

    bump the version, and add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+### v1.3.19
+
+- The "persist-flags" configuration can be enforced for a project in top level MODULE file.
+  This value overrides both the default value and the value specified in a user's config file.
--- util/util.go
+++ util/util.go
@@ -18,7 +18,7 @@ import (
 
 // DbtVersion is the current version of DBT. The minor version
 // is also used as the MODULE file version.
-var DbtVersion = [3]uint{1, 3, 18}
+var DbtVersion = [3]uint{1, 3, 19}
 
 // ModuleFileName is the name of the file describing each module.
 const (
